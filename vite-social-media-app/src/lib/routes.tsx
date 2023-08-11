7130136d2a9e82a1c465825c9ce70f15fa3eebf2
Leprekus <leprekus@yahoo.com>
2023-08-11 12:26:05 -0700

    create search component

--- vite-social-media-app/src/lib/routes.tsx
+++ vite-social-media-app/src/lib/routes.tsx
@@ -2,6 +2,7 @@ import Login from '../pages/Login';
 import Messages from '../pages/auth/Messages';
 import Profile from '../pages/auth/Profile';
 import Home from '../pages/auth/Home';
+import Search from '../pages/auth/Search';
 
 //dynamic routes
 //need to go at the end of 
@@ -9,7 +10,7 @@ import Home from '../pages/auth/Home';
 export default [
     { path: '/', component: <Home/> },
     { path: '/login', component: <Login/> },
-    { path: '/search', component: <Login/> },
+    { path: '/search', component: <Search/> },
     { path: '/messages', component: <Messages/> },
     { path: '/:username', component: <Profile/> },
   ]
