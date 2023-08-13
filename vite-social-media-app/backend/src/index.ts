e64cccebc44a08ba04ca08d1f88a4a8af7730ee4
Leprekus <leprekus@yahoo.com>
2023-08-12 23:27:11 -0700

    conenct app to backend

--- vite-social-media-app/backend/src/index.ts
+++ vite-social-media-app/backend/src/index.ts
@@ -1,7 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 
 const PORT = 4321
 const app = express()
+
+
+app.use(cors())
+app.get('/api', (req: express.Request, res:express.Response) => {
+    res.json({ message: 'hello world '})
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
