df338acf5b3e5cf1211896b2f40cf02fbf315d1b
Leprekus <leprekus@yahoo.com>
2023-08-17 00:44:16 -0700

    remove console log

--- vite-social-media-app/backend/src/api/GET/post.ts
+++ vite-social-media-app/backend/src/api/GET/post.ts
@@ -7,11 +7,10 @@ export default async function handler(req: Request, res: Response) {
     const id = req.query.id
     
     const post = await PostsBucket.getOne().where('id').equals(id).run()
-    console.log({ post, id })
+    
     if(!post)
         return res.status(200).json({ posts: [] })
 
-    //const sortedPosts = sort(posts as IPost[])
     return res.status(200).json({ post })
   
 }
