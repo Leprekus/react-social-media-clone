ea559f5949b73690e1ce4af3eabef2a01a3f40d8
Leprekus <leprekus@yahoo.com>
2023-08-24 05:21:16 -0700

    update cors

--- vite-social-media-app/backend/src/index.ts
+++ vite-social-media-app/backend/src/index.ts
@@ -14,15 +14,7 @@ app.use(cors({
 }))
 
 // Add this middleware to set the CORS headers explicitly.
-app.use((req, res, next) => {
-    console.log({ origin: process.env.ORIGIN_URL, reqUrl: req.headers.origin })
-    res.setHeader('Access-Control-Allow-Origin', process.env.ORIGIN_URL!);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-    
-});
+app.options('*', cors());
 
 app.use(body)
 app.use(cookieParser())
