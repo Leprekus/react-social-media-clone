6b9c2bfe68e01c2a5004e296afff336b236e66a6
Leprekus <leprekus@yahoo.com>
2023-08-24 04:59:29 -0700

    set cors explicitly

--- vite-social-media-app/backend/src/index.ts
+++ vite-social-media-app/backend/src/index.ts
@@ -15,6 +15,15 @@ app.use(cors({
     credentials: true
 }))
 
+// Add this middleware to set the CORS headers explicitly.
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', process.env.ORIGIN_URL!);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
 
 
 
