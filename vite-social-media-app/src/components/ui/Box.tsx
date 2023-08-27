72eb780b7416b2ab651bcefa96abf047155aa72f
Leprekus <leprekus@yahoo.com>
2023-08-27 15:44:06 -0700

    style box component

--- vite-social-media-app/src/components/ui/Box.tsx
+++ vite-social-media-app/src/components/ui/Box.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react'
+import { BsArrowLeftShort } from 'react-icons/bs'
 import { IoIosClose } from 'react-icons/io'
 import { twMerge } from 'tailwind-merge'
 interface BoxProps {
@@ -27,20 +28,36 @@ export default function Box({ children, onClick, className }: BoxProps) {
     overflow-y-hidden
     '>
         <div className={twMerge(`
-          w-[400px]
-          min-h-[500px]
+          sm:w-[400px]
+          sm:h-[500px]
           mx-auto
           text-white
           bg-neutral-900
-          rounded-md
+          sm:rounded-md
           overflow-hidden
+
+          h-screen
+          w-screen
+
+          flex flex-col gap-8
 
           absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
         `, className)}>
-          <button onClick={handleClick} className='absolute right-1 top-1 hover:bg-neutral-800 rounded-md transition z-50'>
-            <IoIosClose className='text-gray-400' size={25}/>
-          </button>
-          { children }
+          <div className='p-4 relative h-10'>
+            <button onClick={handleClick} className=' 
+            sm:absolute 
+            sm:right-2
+            hover:bg-neutral-800 
+            rounded-md 
+            transition 
+            z-50'>
+              <BsArrowLeftShort size={40} className='text-gray-400 sm:hidden'/>
+              <IoIosClose className='text-gray-400 hidden sm:block' size={40}/>
+            </button>
+          </div>
+          <div>
+            { children }
+          </div>
         </div>
     </div>
   )
